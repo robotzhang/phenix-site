4b8c35f5502a3733f745090a3c3ef770e79c2837
Robot.Zhang <robotzhang@qq.com>
2025-12-08 14:06:28 +0800

    for auto deploy

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -25,7 +25,7 @@ export default function Home({ loaderData }: Route.ComponentProps) {
         <Buy />
       </div>
 
-      <div className="py-10 sm:py-20 max-w-5xl m-auto">
+      <div className="py-10 sm:py-20 max-w-5xl m-auto" title="fqa section">
         <h2 className="text-3xl font-semibold mb-2 sm:mb-6">FQA</h2>
         <FQA />
       </div>

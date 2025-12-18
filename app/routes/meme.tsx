a4ba9b15552f74fc6894cd0524c4a23c2a739a73
Robot.Zhang <robotzhang@qq.com>
2025-12-18 15:53:11 +0800

    meme faq

--- app/routes/meme.tsx
+++ app/routes/meme.tsx
@@ -1,10 +1,17 @@
+import MemeFaq from "./Meme/Faq";
+
 export default function Meme() {
   return (
     <div>
-      <div>prices</div>
-      <div>buy</div>
+      <div>price chart</div>
+      <div>buy form</div>
       <div>recent buy</div>
-      <div>faq</div>
+      
+      <div className="py-10 sm:py-20 max-w-5xl m-auto">
+        <h2 className="text-3xl font-semibold mb-2 sm:mb-6">FQA</h2>
+        <MemeFaq />
+      </div>
+      
       <div>exchange</div>
     </div>
   );

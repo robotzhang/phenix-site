0582b926c0ac2c47d77823f759479db709ab8089
Robot.Zhang <robotzhang@qq.com>
2026-01-14 04:16:01 +0800

    use ALCHEMY node api

--- app/components/wallet/wagmi.ts
+++ app/components/wallet/wagmi.ts
@@ -1,10 +1,25 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { base, baseSepolia, mainnet } from 'wagmi/chains';
+import { http } from 'wagmi';
+import { mainnet, base, baseSepolia } from 'wagmi/chains';
+
+const ALCHEMY_KEY = 'tHUSSiHRPRvWVlhjE7SML';
 
 export const config = getDefaultConfig({
   appName: 'Phenix',
   projectId: '3893e093df3f96ab4f13228a34c8e962',
   chains: [mainnet, base, baseSepolia],
+
+  transports: {
+    [mainnet.id]: http(`https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`, {
+      batch: true,
+    }),
+    [base.id]: http(`https://base-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`, {
+      batch: true,
+    }),
+    [baseSepolia.id]: http(`https://base-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`, {
+      batch: true,
+    }),
+  },
+
   ssr: true,
 });
-
